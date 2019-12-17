e34fa0ffcfa58c63b95a962b32b4d863beb8c746
SANDRAJG <58989388+SANDRAJG@users.noreply.github.com>
2019-12-17 14:47:24 -0500

    Add files via upload

---
+++ edad_joven_viejo.py
@@ -0,0 +1,22 @@
+DETERMINAR SI ES JOVEN O VIEJO UNA PERSONA.
+
+while True:
+    firstname = input("Cuál es su primer nombre? ")
+    lastname = input("Cuál es su apellido? ")
+    location = input("Cuál es su dirección? ")
+    age = input("Cuál es su edad? ")
+    x=int(age)
+    space = " "
+    print("Hola" + space + firstname + space + lastname + ", tienes" + space + age + space + "años y vives en" + space + location)
+    if x > 1 and x <= 12:
+        print("*** Usted es un niño")
+    elif x > 12 and x <= 18:
+        print("*** Usted es un adolescente")
+    elif x > 18 and x <= 65:
+        print("*** Usted es un adulto")
+    else:
+        print("*** Usted es un adulto mayor")
+    salir = input("Desea salir? [S] o [N]")
+    if salir == 'S':
+        break
+print ("Gracias")
