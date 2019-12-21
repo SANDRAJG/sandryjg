3f9ee9bfe85bf0a3eed2ce051f0b0b3608c970f1
SANDRAJG <58989388+SANDRAJG@users.noreply.github.com>
2019-12-21 09:14:31 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Dec 13 19:22:02 2019
+Created on Fri Dec 13 18:26:18 2019
 
 @author: CEC
 """
 
-import fibo1
-fibo1.fib(100)
+import module
---
+++ module.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 13 18:27:18 2019
+
+@author: CEC
+"""
+"""print("Quiero ser un modulo")"""
+
+if __name__=="__main__":
+    print("I prefer to be a module")
+else:
+    print("I like to be a module")
