0ffa9d02cc24b808afa5d5bb015fd10725a2458e
SANDRAJG <58989388+SANDRAJG@users.noreply.github.com>
2019-12-21 09:10:37 -0500

    Add files via upload

---
+++ prueba1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 23 10:29:15 2019
+
+@author: CEC
+"""
+
+print("hOLA MUNDO!")
+a=1
+b=2
+x=3
+print("This value of X is"+ x)
+str1="Cisco"
+str2="Networking"
+str3="Academy"
+x=3
+type(x)
+pi = 22/7
+print(pi)
